962e901cb90454276e96ea4beb56ac0f74efe9e8
Yunay Hamza <yunay@abv.bg>
2018-12-06 08:58:01 +0200

    added first game blank page

---
+++ client/src/games/saboteur/home.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export class Saboteur extends React.Component<{},{}>{
+    
+    public render(){
+
+        return <div>Саботьор</div>
+    }
+}
--- client/src/ui/pages/Games.tsx
+++ client/src/ui/pages/Games.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react'
+import { Saboteur } from 'games'
+import { Route } from 'react-router';
+import { Link } from 'react-router-dom';
 
 export default class Games extends React.Component<{},{}>{
     
     public render(){
 
-        return <div>Игри</div>
+        return <div>
+            <ul>
+                <li>
+                    <Link to="/games/saboteur">Саботьор</Link>
+                </li>
+            </ul>
+
+             <Route path="/games/saboteur" component={Saboteur} />
+
+        </div>
     }
 }
