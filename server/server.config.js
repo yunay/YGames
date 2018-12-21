0777a1efa846860610f4945996c46fd8f86144b2
Yunay Hamza <yunay@abv.bg>
2018-12-21 21:10:02 +0200

    added configs

---
+++ server/server.config.js
@@ -0,0 +1,6 @@
+const serverConfig = {
+    connectionString: 'mongodb://yunay:asd123456@ds261253.mlab.com:61253/ygames',
+    secret:'ortemcv11mk12mn4n6nuj7pp11p'
+}
+
+module.exports = serverConfig;
