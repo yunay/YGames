43a7a0785df35f5ab42377ccd710ac919962cdba
yunay-laptop <yunay@abv.bg>
2018-12-23 21:06:01 +0200

    lobby

--- client/src/common/ui/active-users.tsx
+++ client/src/common/ui/active-users.tsx
@@ -13,38 +13,47 @@ export class ActiveUsers extends React.Component<{}, {}>{
             <tbody>
                 <tr>
                     <td>Юнай</td>
+                    <td><span className="badge badge-success">Свободен</span></td>
                     <td><button type="button" className="btn btn-danger btn-sm">Откажи <i className="fa fa-user-times"></i></button></td>
                 </tr>
                 <tr>
                     <td>Джей</td>
+                    <td><span className="badge badge-success">Свободен</span></td>
                     <td><button type="button" className="btn btn-success btn-sm">Покани <i className="fa fa-user-plus"></i></button></td>
                 </tr>
                 <tr>
                     <td>Джей</td>
+                    <td><span className="badge badge-success">Свободен</span></td>
                     <td><button type="button" className="btn btn-success btn-sm">Покани <i className="fa fa-user-plus"></i></button></td>
                 </tr>
                 <tr>
                     <td>Джей</td>
+                    <td><span className="badge badge-danger">Зает</span></td>
                     <td><button type="button" className="btn btn-success btn-sm">Покани <i className="fa fa-user-plus"></i></button></td>
                 </tr>
                 <tr>
                     <td>Джей</td>
+                    <td><span className="badge badge-danger">Зает</span></td>
                     <td><button type="button" className="btn btn-success btn-sm">Покани <i className="fa fa-user-plus"></i></button></td>
                 </tr>
                 <tr>
                     <td>Джей</td>
+                    <td><span className="badge badge-danger">Зает</span></td>
                     <td><button type="button" className="btn btn-success btn-sm">Покани <i className="fa fa-user-plus"></i></button></td>
                 </tr>
                 <tr>
                     <td>Джей</td>
+                    <td><span className="badge badge-danger">Зает</span></td>
                     <td><button type="button" className="btn btn-success btn-sm">Покани <i className="fa fa-user-plus"></i></button></td>
                 </tr>
                 <tr>
                     <td>Джей</td>
+                    <td><span className="badge badge-danger">Зает</span></td>
                     <td><button type="button" className="btn btn-success btn-sm">Покани <i className="fa fa-user-plus"></i></button></td>
                 </tr>
                 <tr>
                     <td>Джей</td>
+                    <td><span className="badge badge-danger">Зает</span></td>
                     <td><button type="button" className="btn btn-success btn-sm">Покани <i className="fa fa-user-plus"></i></button></td>
                 </tr>
             </tbody>
--- client/src/common/ui/lobby.tsx
+++ client/src/common/ui/lobby.tsx
@@ -2,22 +2,68 @@ import * as React from 'react'
 import { Chat } from './chat'
 import { ActiveUsers } from './active-users'
 import { Rooms } from './rooms'
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
 
-export class Lobby extends React.Component<{}, {}>{
+@observer export class Lobby extends React.Component<{}, {}>{
+    @observable private shownGameRules:boolean;
+
+    constructor(props:any){
+        super(props);
+
+        this.shownGameRules = false;
+        this.toggleGameRules = this.toggleGameRules.bind(this);
+    }
 
     public render() {
 
-        return <div><div className="row">
-            <div className="col-4">
-                <Chat />
+        return <div>
+            <div className="row">
+                <div className="col-md-7">
+                    <h2 className="lobby-main-header">Добре дошли!</h2>
+                    <h3 className="lobby-main-header">Приятна игра на Саботьор!</h3>
+                </div>
+                <div className="col-md-5">
+                    <div className="btn-group mr-2" role="group">
+                        <button type="button" className="btn btn-info btn-md lobby-main-btn" onClick={this.toggleGameRules}>
+                        <i className="fa fa-info-circle"></i>Правила на играта</button>
+                        <button type="button" className="btn btn-success btn-md lobby-main-btn">
+                        <i className="fa fa-play-circle-o"></i>Създай стая за игра</button>
+                    </div>
+                </div>
             </div>
-            <div className="col-4">
-                <ActiveUsers />
+            <div className="row">
+                <div className="col-4">
+                    <Chat />
+                </div>
+                <div className="col-4">
+                    <ActiveUsers />
+                </div>
+                <div className="col-4">
+                    <Rooms />
+                </div>
             </div>
-            <div className="col-4">
-                <Rooms />
+            <div className="row">
+                <div className={`modal fade ${this.shownGameRules ? "show" : ""}`} role="dialog" style={this.shownGameRules ? {display: "block", paddingRight: "17px"}: {}}>
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLongTitle">Правила на играта</h5>
+                                <button type="button" className="close" onClick={this.toggleGameRules}>
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                ...
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        </div>
+    }
+
+    private toggleGameRules(){
+        this.shownGameRules = !this.shownGameRules;
     }
 }
